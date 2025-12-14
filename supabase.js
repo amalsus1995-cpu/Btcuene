0434c5372befa566c8c96618d7c4f45bd09dcf12
amalsus1995-cpu <am.alsus1995@gmail.com>
2025-12-14 22:46:18 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 
-<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
-<script>
-  const supabaseUrl = "...";
-  const supabaseKey = "...";
-  const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
-</script>
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
+
+const supabaseUrl = "https://gpfxakuneeuafxgqbobs.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwZnhha3VuZWV1YWZ4Z3Fib2JzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU3MzE3ODEsImV4cCI6MjA4MTMwNzc4MX0.x2Mh3LwIg-8peEpJlif_1RcbrA-CyGa-xjtESaYKcXQ";
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
